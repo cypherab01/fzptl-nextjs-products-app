edd014dea7460eda5dc7384daf1d4aeb34e5e099
cypherab01 <aghimire074@gmail.com>
2025-12-04 11:43:21 +0545

    misc

--- components/product/ProductCardSkeleton.tsx
+++ components/product/ProductCardSkeleton.tsx
@@ -42,7 +42,7 @@ const ProductCardSkeleton = () => {
 
 const ProductsCardSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
       {Array(6)
         .fill(null)
         .map((_, index) => (
